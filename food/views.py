10562e131f4ee2b7c3c2a250d9777e4e292f43b7
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2019-11-02 23:13:46 +0530

    Add files via upload

--- food/views.py
+++ food/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
 from django.template import loader
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import ItemForm
 # Create your views here.
 def index(req):
     item_list=Item.objects.all()
@@ -23,3 +22,29 @@ def detail(request,item_id):
     }
     # return HttpResponse("This is item no/id: %s"%item_id)
     return render(request, 'food/detail.html ',context)
+
+def create_item(request):
+    form=ItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request, 'food/item-form.html',{'form':form})
+
+
+def update_item(request,id):
+    item=Item.objects.get(id=id)
+    form=ItemForm(request.POST or None,instance=item)
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    return render(request,'food/item-form.html',{'form':form,'item':item})
+
+def delete_item(request,id):
+    item=Item.objects.get(id=id)
+
+    if request.method =='POST':
+        item.delete()
+        return redirect('food:index')
+    
+    return render(request,'food/item-delete.html',{'item':item})
